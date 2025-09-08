ea83c33ed26fb4db2cc42f250d26f58e35355710
Krishnpal Mewada <kpmewada9174@gmail.com>
2025-09-08 16:56:00 +0000

    not on;ly chatbot but when i use facilities like parking hotels and all

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -248,7 +248,7 @@ export function Dashboard() {
           </div>
         </Header>
         <div className="flex-1 relative">
-          <div className={cn('w-full h-full', { 'invisible': isChatbotOpen })}>
+           <div className={cn('w-full h-full', { 'invisible': isChatbotOpen || activeFilter !== 'all' })}>
             <MapView
                 facilities={filteredFacilities}
                 onSelectFacility={handleSelectFacility}
